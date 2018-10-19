be8b39b30c0b519dfd3dbb1fc8cd3556cdca9cbe
yangnewman <wydhmy@163.com>
2018-10-19 16:30:35 +0800

    再次修改

--- boke1/boke/home/views.py
+++ boke1/boke/home/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from home.models import UserArticle
@@ -20,9 +21,30 @@ def about(request):
 
 def info(request):
     if request.method == 'GET':
-        articles = UserArticle.objects.all()
-        return render(request, 'info.html', {'articles':articles})
 
+        articles = UserArticle.objects.all()
+        paginator = Paginator(articles, 1)
+        page_num = int(request.GET.get('page', 1))
+        page_num_next = page_num + 1
+        page_num_previous = page_num - 1
+
+        # 当前页内容
+        pages= paginator.page(page_num)
+        # 总页码
+        pages_all = pages.paginator.num_pages
+        # 下一页内容
+        if page_num_next > pages_all:
+            pages_next = ''
+        else:
+            pages_next = paginator.page(page_num_next)
+        # 上一页内容
+        if page_num_previous:
+            pages_previous = paginator.page(page_num_previous)
+        else:
+            pages_previous = ''
+        return render(request, 'info.html', {'pages':pages,
+                                             'pages_next':pages_next,
+                                             'pages_previous':pages_previous})
 
 
 
